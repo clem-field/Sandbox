667fead26f676d00a83981310ce22dbe733ff51c
clem-field <kc8yhe@me.com>
2025-08-20 11:31:34 -0500

    cleaning up files

---
+++ service.tf
@@ -0,0 +1,20 @@
+resource "aws_ecs_service" "service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  tags = { Name = "ecs-service" }
+
+  network_configuration {
+    subnets         = [aws_subnet.public1.id, aws_subnet.public2.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+}
